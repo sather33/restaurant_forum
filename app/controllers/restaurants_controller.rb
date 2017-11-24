b58a80819c44e1e3d3719a1b04715d6ac9b606c0
Kevin_wu <court.dream3@gmail.com>
2017-11-24 20:54:29 +0800

    use css in front-index

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,21 @@
 class RestaurantsController < ApplicationController
+  #before_action :authenticate_user!
 
+  before_action :set_restaurant, only: [:show]
   def index
-
+    @restaurants = Restaurant.page(params[:page]).per(10)
   end
+
+  # def show
+  #   #@restaurant = Restaurant.find(params[:id])
+  # end
+
+
+
+
+  private
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+  
 end
