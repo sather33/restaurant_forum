971000a09de8af289f2010f661a7daec174b3481
Kevin_wu <court.dream3@gmail.com>
2017-11-24 19:33:35 +0800

    add photo upload using Carrierwave gem

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -42,7 +42,7 @@ class Admin::RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
   end
   def restaurant_params
-    params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description)
+    params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description,:image)
   end
 
 end
