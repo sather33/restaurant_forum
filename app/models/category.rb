791ae301d297d103c40108b24c0ca129312dd472
sather <court.dream3@gmail.com>
2017-12-03 19:47:40 +0800

    update relationship

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,3 @@
 class Category < ApplicationRecord
-  has_many :restaurants
+  has_many :restaurants, dependent: :destroy
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
-  belongs_to :category
+  belongs_to :category, optional: true
   validates_presence_of :name
   mount_uploader :image, PhotoUploader
 end
